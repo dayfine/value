0217d76daf9d82a109d60b46d51c9e79ca5c29d2
dayfine <difancpa@gmail.com>
2019-01-27 17:28:00 -0800

    initial eslint for js files

--- webpack.config.js
+++ webpack.config.js
@@ -3,26 +3,26 @@ const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  entry: {
-	  app: ['./app/index.tsx', 'webpack-hot-middleware/client'],
-	  vendor: ['react', 'react-dom']
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'js/[name].bundle.js'
-  },
-  devtool: 'source-map',
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js', 'jsx', '.json']
-  },
-  module: {
-    rules: [
-      { test: /\.tsx?$/, loader: 'awesome-typescript-loader' },
-      { enforce: 'pre', test: /\.js$/, loader: 'source-map-loader' }
-    ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'index.html') }),
-    new webpack.HotModuleReplacementPlugin()
-  ]
+    entry: {
+        app: ['./app/index.tsx', 'webpack-hot-middleware/client'],
+        vendor: ['react', 'react-dom'],
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/[name].bundle.js',
+    },
+    devtool: 'source-map',
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', 'jsx', '.json'],
+    },
+    module: {
+        rules: [
+            { test: /\.tsx?$/, loader: 'awesome-typescript-loader' },
+            { enforce: 'pre', test: /\.js$/, loader: 'source-map-loader' },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'index.html') }),
+        new webpack.HotModuleReplacementPlugin(),
+    ],
 }
