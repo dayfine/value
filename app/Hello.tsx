8d647b31b4a301f839243afa571bfd918a9a8843
dayfine <difancpa@gmail.com>
2019-01-27 19:13:32 -0800

    react with hook!

--- app/Hello.tsx
+++ app/Hello.tsx
@@ -1,9 +1,13 @@
-import * as React from "react"
+import React from "react"
+import HookEx from "./HookEx"
 
 export interface IHelloProps { compiler: string; framework: string }
 
 const Hello = (props: IHelloProps) => (
-    <h1>Hello from {props.compiler} and {props.framework}!</h1>
+    <div>
+        <h1>Hello from {props.compiler} and {props.framework}!</h1>
+        <HookEx />
+    </div>
 )
 
 export default Hello
---
+++ app/HookEx.tsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+function HookEx(): JSX.Element {
+  // Declare a new state variable, which we'll call "count"
+  const [count, setCount] = React.useState<number>(0)
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me!!
+      </button>
+    </div>
+  )
+}
+
+export default HookEx
